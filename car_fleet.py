0a5d42befd8b5e74686ed99240668c74f0e62162
kshah117 <shahkevin1107@gmail.com>
2024-07-21 16:39:40 +0530

    finished car fleet

---
+++ car_fleet.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        ds = []
+        cf = 0
+        for i in range(len(position)):
+            ds.append((position[i], speed[i]))
+
+        ds.sort(reverse=True)
+        stack = []
+        for c in ds:
+            time = (target - c[0]) / c[1]
+            stack.append(time)
+
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
