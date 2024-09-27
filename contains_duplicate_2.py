972ff4528fd49f82593b445dd6d912bffedf65cc
kshah117 <shahkevin1107@gmail.com>
2024-09-27 11:49:57 +0530

    optimized contains duplicate 2 solution

--- contains_duplicate_2.py
+++ contains_duplicate_2.py
@@ -1,23 +1,14 @@
-from typing import List
-
-
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         temp = {}
 
         for i in range(len(nums)):
             if nums[i] not in temp:
-                temp[nums[i]] = [i]
+                temp[nums[i]] = i
                 continue
             
-            if abs(temp[nums[i]][-1] - i) <= k:
+            if abs(temp[nums[i]] - i) <= k:
                 return True
             else:
-                temp[nums[i]].append(i)
-            
+                temp[nums[i]] = i
         return False
-    
-sol = Solution()
-nums = [1,0,1,1]
-k = 1
-print(sol.containsNearbyDuplicate(nums, k))
